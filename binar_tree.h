92f151dc8c9f3f43ec2343ae65622dfdcf643460
ksmasteru <142666033+ksmasteru@users.noreply.github.com>
2024-05-02 16:30:38 +0100

    done

---
+++ binar_tree.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_TREE
+#define BINARY_TREE
+
+typedef strcut s_tree{
+  int data;
+  struct s_tree right;
+  struct s_tree left;
+}t_tree;
+
+
---
+++ insert_binary_tree.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_node{
+  int data;
+  struct s_node *left;
+  struct s_node *right;
+}t_node;
+
+t_node *insert(t_node *root, int data)
+{
+  if (root == NULL)
+  {
+    root = malloc(sizeof(t_node));
+    root->data = data;
+    root->left = NULL;
+    root->right = NULL;
+    return (root);
+  }
+  if (data > root->data) /* traveling right*/
+  {
+    root->right = insert(root->right, data);
+  }
+  else if (data < root->data)
+  {
+    root->left = insert(root->left, data);
+  }
+  return (root);
+}
+int main()
+{
+  t_node *root;
+  root = insert(0, 15);
+  insert(root, 5);
+  insert(root, 10);
+  insert(root, 2);
+  printf("%d", root->left->left->data);
+
+} 
