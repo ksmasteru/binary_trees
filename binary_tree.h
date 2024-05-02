424368c60e1e33012ef734d07f22c6863505ae17
ksmasteru <142666033+ksmasteru@users.noreply.github.com>
2024-05-02 17:18:47 +0100

    new

---
+++ binary_tree.h
@@ -0,0 +1,14 @@
+#ifndef BINARY_TREE
+#define BINARY_TREE
+
+#define TRUE 0
+#define FALSE 1
+typedef struct s_root{
+  int data;
+  struct s_root *right;
+  struct s_root *left;
+}t_root;
+
+#endif
+
+
---
+++ searh_BT.c
@@ -0,0 +1,51 @@
+#include "binary_tree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void searchBinary(t_root **root, int data)
+{
+    if (*root == NULL)
+    {
+        printf("NOT FOUND");
+        return ;
+    }
+    else if ((*root)->data == data)
+    {
+        printf("FOUND!");
+        exit(0);
+    }
+    else if ((*root)->data < data)
+        searchBinary(&((*root)->right), data);
+    else if ((*root)->data > data)
+        searchBinary(&((*root)->left), data);
+}
+t_root *insert(t_root **root, int data)
+{
+  if (*root == NULL)
+  {
+    (*root) = malloc(sizeof(t_root));
+    (*root)->data = data;
+    (*root)->left = NULL;
+    (*root)->right = NULL;
+    return (*root);
+  }
+  if (data > (*root)->data) /* traveling right*/
+  {
+    (*root)->right = insert(&((*root)->right), data);
+  }
+  else if (data < (*root)->data)
+  {
+    (*root)->left = insert(&((*root)->left), data);
+  }
+  return (*root);
+}
+int main(int ac, char **av)
+{
+  t_root *root = NULL;
+  insert(&root, 5);
+  insert(&root, 10);
+  insert(&root, 2);
+  insert(&root, 20);
+  searchBinary(&root, atoi(av[1]));
+}
